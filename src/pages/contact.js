1dbbda4c123f45afda7332b0954b00fe5e4e6519
BeRi <iberi22@gmail.com>
2019-08-28 23:35:47 -0500

    sync scafold gastby, graphql

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const constac = () => {
+  return (
+    <div>
+      <h1>INFORMACION DE CONTACTO</h1>
+      <h2>menu del dia</h2>
+      <h3>postres especiales</h3>
+    </div>
+  )
+}
+export default constac
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const reslt = () => {
+  return (
+    <div>
+      <h1>gusto y sabor</h1>
+      <h2>menu del dia</h2>
+      <h3>postres especiales</h3>
+    </div>
+  )
+}
+export default reslt
